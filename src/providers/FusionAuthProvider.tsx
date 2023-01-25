ae75ff232360bf9957449f5094feee2438fbc725
Brent Halsey <brent.halsey@fusionauth.io>
2023-01-25 14:18:00 -0700

    WIP: work with magic backend pieces

--- src/providers/FusionAuthProvider.tsx
+++ src/providers/FusionAuthProvider.tsx
@@ -43,67 +43,89 @@ export interface FusionAuthConfig extends PropsWithChildren {
     onRedirectSuccess?: RedirectSuccess;
     onRedirectFail?: RedirectFail;
     scope?: string;
+
+    loginPath?: string;
+    logoutPath?: string;
+    registerPath?: string;
+    tokenRefreshPath?: string;
+    mePath?: string;
 }
 
 export const FusionAuthProvider: React.FC<FusionAuthConfig> = props => {
     const { children } = props;
 
     const [isAuthenticated, setIsAuthenticated] = useState(
-        () => !!Cookies.get('user'),
+        () => !!Cookies.get('access_token_expires'), // TODO - look at idToken?
     );
     const [isLoading, setIsLoading] = useState(false);
 
     const [user, setUser] = useState<Record<string, any>>({});
 
-    const generateUrl = useCallback(
-        (functionType: FunctionType, queryParams: Record<string, string>) => {
+    const generateOauthUrl = useCallback(
+        (
+            oauthFunctionType: OauthFunctionType,
+            queryParams?: Record<string, string>,
+        ) => {
             const query = new URLSearchParams(queryParams);
+            const queryString = query.toString().length > 0 ? `?${query}` : '';
 
-            return `${props.baseUrl}/oauth2/${functionType}?${query}`;
+            return `${props.baseUrl}/oauth2/${oauthFunctionType}${queryString}`;
         },
         [props.baseUrl],
     );
 
+    const generateServerUrl = useCallback(
+        (
+            serverEndpoint: ServerFunctionType,
+            propPathOverride?: string,
+            queryParams?: Record<string, string>,
+        ) => {
+            const query = new URLSearchParams(queryParams);
+            const queryString = query.toString().length > 0 ? `?${query}` : '';
+            const path = propPathOverride
+                ? propPathOverride
+                : `/app/${serverEndpoint}`;
+            // TODO use mapping config
+            return `${props.serverUrl}${path}${queryString}`;
+        },
+        [props.serverUrl],
+    );
+
     const login = useCallback(
         async (state = '') => {
-            const stateParam = `${generateRandomString()}:${state}`;
-            Cookies.set('lastState', stateParam);
-            const code = await generatePKCE();
-            Cookies.set('codeVerifier', code.code_verifier);
-            const queryParams = {
-                client_id: props.clientID,
-                scope: props?.scope ?? DEFAULT_SCOPE,
-                response_type: 'code',
-                redirect_uri: props.redirectUri,
-                code_challenge: code.code_challenge,
-                code_challenge_method: 'S256',
-                state: stateParam,
-            };
-            const fullUrl = generateUrl(FunctionType.login, queryParams);
+            // const stateParam = `${generateRandomString()}:${state}`;
+            // Cookies.set('lastState', stateParam);
+            const fullUrl = generateServerUrl(
+                ServerFunctionType.login,
+                props?.loginPath,
+                { client_id: props.clientID },
+            );
             window.location.assign(fullUrl);
         },
-        [generateUrl, props.clientID, props.redirectUri, props?.scope],
+        [generateOauthUrl, props.clientID, props.redirectUri, props?.scope], // TODO be accurate
     );
 
     const logout = useCallback(async () => {
         // Clear cookies
         Cookies.remove('user');
         Cookies.remove('lastState');
-        Cookies.remove('codeVerifier');
 
-        fetch(`${props.serverUrl}/logout`, { credentials: 'include' }).then(
-            () => {
-                const queryParams = {
-                    client_id: props.clientID,
-                    post_logout_redirect_uri: props.redirectUri,
-                    id_token_hint: props.idTokenHint ?? '',
-                };
-                const fullUrl = generateUrl(FunctionType.logout, queryParams);
-                window.location.assign(fullUrl);
-            },
-        );
+        fetch(generateServerUrl(ServerFunctionType.logout, props?.logoutPath), {
+            credentials: 'include',
+        }).then(() => {
+            const queryParams = {
+                client_id: props.clientID,
+                post_logout_redirect_uri: props.redirectUri,
+                id_token_hint: props.idTokenHint ?? '',
+            };
+            const fullUrl = generateOauthUrl(
+                OauthFunctionType.logout,
+                queryParams,
+            );
+            window.location.assign(fullUrl);
+        });
     }, [
-        generateUrl,
+        generateOauthUrl,
         props.clientID,
         props.idTokenHint,
         props.redirectUri,
@@ -112,77 +134,86 @@ export const FusionAuthProvider: React.FC<FusionAuthConfig> = props => {
 
     const register = useCallback(
         async (state = '') => {
-            const stateParam = `${generateRandomString()}:${state}`;
-            Cookies.set('lastState', stateParam);
-            const code = await generatePKCE();
-            Cookies.set('codeVerifier', code.code_verifier);
-            const queryParams = {
-                client_id: props.clientID,
-                scope: props.scope ?? DEFAULT_SCOPE,
-                response_type: 'code',
-                redirect_uri: props.redirectUri,
-                code_challenge: code.code_challenge,
-                code_challenge_method: 'S256',
-                state: stateParam,
-            };
-            const fullUrl = generateUrl(FunctionType.register, queryParams);
+            // const stateParam = `${generateRandomString()}:${state}`;
+            // Cookies.set('lastState', stateParam);
+            const fullUrl = generateServerUrl(
+                ServerFunctionType.register,
+                props?.registerPath,
+                { client_id: props.clientID },
+            );
             window.location.assign(fullUrl);
         },
-        [generateUrl, props.clientID, props.redirectUri, props.scope],
+        [generateOauthUrl, props.clientID, props.redirectUri, props.scope], // TODO be accurate
     );
 
     useEffect(() => {
         const userCookie = Cookies.get('user');
         if (userCookie) {
-            setUser(JSON.parse(userCookie));
+            try {
+                setUser(JSON.parse(userCookie));
+            } catch {
+                // ignore errors
+            }
         }
     }, [setUser]);
 
-    const refreshToken = useCallback(async () => {
-        await fetch(`${props.serverUrl}/jwt-refresh`, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            credentials: 'include',
-        });
-    }, [props.serverUrl]);
-
-    useLayoutEffect(() => {
-        const lastState = Cookies.get('lastState');
-        const codeVerifier = Cookies.get('codeVerifier');
-
-        if (hasAuthParams() && lastState !== null) {
-            const urlParams = new URLSearchParams(window.location.search);
-
-            if (lastState === urlParams.get('state')) {
-                setIsLoading(true);
-                fetch(`${props.serverUrl}/token-exchange`, {
+    const refreshToken = async () => {
+        const accessTokenExpires = Cookies.get('access_token_expires');
+        if (
+            accessTokenExpires === undefined ||
+            Number(accessTokenExpires) < Date.now() + 60000
+        ) {
+            // TODO use constant
+            await fetch(
+                generateServerUrl(
+                    ServerFunctionType.tokenRefresh,
+                    props?.tokenRefreshPath,
+                ),
+                {
                     method: 'POST',
-                    body: JSON.stringify({
-                        code: urlParams.get('code'),
-                        code_verifier: codeVerifier,
-                    }),
-                    headers: {
-                        'content-type': 'application/json',
-                    },
                     credentials: 'include',
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        Cookies.set('user', JSON.stringify(data.user));
-                        setUser(data.user);
-                        setIsAuthenticated(true);
+                },
+            );
+        }
+    }; // TODO - keep value in props?
 
-                        const [, ...stateParam] = lastState.split(':');
-                        const state = stateParam.join(':');
-                        props.onRedirectSuccess?.(state);
-                    })
-                    .catch(error => {
-                        props.onRedirectFail?.(error);
-                    })
-                    .finally(() => setIsLoading(false));
-            }
+    // TODO - keep this logic in useLayoutEffect()?
+    useLayoutEffect(() => {
+        console.log('in useLayoutEffect');
+        // TODO - get info from url location?  cookies?  how about user info?
+        const lastState = Cookies.get('lastState');
+
+        console.log(
+            `access_token_expires is ${Cookies.get('access_token_expires')}`,
+        );
+        console.log(`user is ${Cookies.get('user')}`);
+        // TODO - difference between this and isAuthenticated?  check for id_token?
+
+        // if (hasAuthParams() && lastState !== undefined && lastState !== null) {
+        if (Cookies.get('access_token_expires') && !Cookies.get('user')) {
+            console.log('in useLayoutEffect()  getting user....');
+
+            const urlParams = new URLSearchParams(window.location.search);
+            setIsAuthenticated(true);
+            setIsLoading(false);
+
+            fetch(generateServerUrl(ServerFunctionType.me, props?.mePath), {
+                credentials: 'include',
+            })
+                .then(response => response.json())
+                .then(user => {
+                    Cookies.set('user', JSON.stringify(user));
+                    setUser(user);
+                    setIsAuthenticated(true);
+
+                    // const [, ...stateParam] = lastState.split(':');
+                    // const state = stateParam.join(':');
+                    // props.onRedirectSuccess?.(state);
+                })
+                .catch(error => {
+                    props.onRedirectFail?.(error);
+                })
+                .finally(() => setIsLoading(false));
         }
     }, [props, props.serverUrl]);
 
@@ -216,48 +247,27 @@ export const FusionAuthProvider: React.FC<FusionAuthConfig> = props => {
 
 export const useFusionAuth = () => useContext(FusionAuthContext);
 
-enum FunctionType {
+enum OauthFunctionType {
     login = 'authorize',
     logout = 'logout',
     register = 'register',
 }
 
-function dec2hex(dec: number) {
-    return ('0' + dec.toString(16)).substr(-2);
-}
-
-async function generatePKCE() {
-    const code_verifier = generateRandomString();
-
-    const encoder = new TextEncoder();
-    const data = encoder.encode(code_verifier);
-    const sha256 = await window.crypto.subtle.digest('SHA-256', data);
-
-    let str = '';
-    const bytes = new Uint8Array(sha256);
-    const len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-        str += String.fromCharCode(bytes[i]);
-    }
-
-    const code_challenge = btoa(str)
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-
-    return { code_verifier, code_challenge };
-}
-
-function generateRandomString() {
-    const array = new Uint32Array(56 / 2);
-    window.crypto.getRandomValues(array);
-    return Array.from(array, dec2hex).join('');
+enum ServerFunctionType {
+    login = 'login',
+    logout = 'logout',
+    register = 'register',
+    tokenRefresh = 'token-refresh',
+    me = 'me',
 }
 
 function hasAuthParams(): boolean {
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get('code');
     const state = searchParams.get('state');
+    const userState = searchParams.get('userState'); // TODO keep?
     const error = searchParams.get('error');
-    return code !== null || state !== null || error !== null;
+    return (
+        code !== null || state !== null || userState !== null || error !== null
+    );
 }
