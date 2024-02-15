4529d3a1b4aa2e6c634f691b5bfd3b9dc41b3b04
Jake Loew <jake@madebychalk.com>
2024-02-15 09:57:12 -0700

    fixes usage of waitFor to render component tests

--- src/__tests__/FusionAuthLogoutButton.test.tsx
+++ src/__tests__/FusionAuthLogoutButton.test.tsx
@@ -1,38 +1,23 @@
 import React from 'react';
-import { screen, render, fireEvent, waitFor } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import { FusionAuthLogoutButton } from '../components/FusionAuthLogoutButton';
 import { FusionAuthProvider } from '../providers/FusionAuthProvider';
 import { mockUseFusionAuth } from './mocks/mockUseFusionAuth';
 import { TEST_CONFIG } from './mocks/testConfig';
 
 describe('FusionAuthLogoutButton', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
-    test('Logout buttons renders the correct text', async () => {
-        await renderProvider();
-        expect(await screen.findByText('Logout')).toBeInTheDocument();
-    });
-
-    test('Logout button will call the useFusionAuth hook', async () => {
+    test('Logout button will call the useFusionAuth hook', () => {
         const logout = jest.fn();
         mockUseFusionAuth({ logout });
 
-        await renderProvider();
-
-        fireEvent.click(screen.getByText('Logout'));
-
-        expect(logout).toBeCalledWith();
-    });
-});
-
-const renderProvider = () => {
-    return waitFor(() =>
         render(
             <FusionAuthProvider {...TEST_CONFIG}>
                 <FusionAuthLogoutButton />
             </FusionAuthProvider>,
-        ),
-    );
-};
+        );
+
+        fireEvent.click(screen.getByText('Logout'));
+
+        expect(logout).toHaveBeenCalled();
+    });
+});
--- src/__tests__/FusionAuthRegisterButton.test.tsx
+++ src/__tests__/FusionAuthRegisterButton.test.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
-import { screen, render, fireEvent, waitFor } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import { FusionAuthRegisterButton } from '../components/FusionAuthRegisterButton';
 import { FusionAuthProvider } from '../providers/FusionAuthProvider';
 import { mockUseFusionAuth } from './mocks/mockUseFusionAuth';
 import { TEST_CONFIG } from './mocks/testConfig';
 
 describe('FusionAuthRegisterButton', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
-    test('Register buttons renders the correct text', async () => {
-        await renderProvider();
-        expect(await screen.findByText('Register Now')).toBeInTheDocument();
-    });
-
     test('Register button will call the useFusionAuth hook', async () => {
         const register = jest.fn();
         mockUseFusionAuth({ register });
 
-        await renderProvider();
+        const stateValue = 'state-value-for-register';
+        render(
+            <FusionAuthProvider {...TEST_CONFIG}>
+                <FusionAuthRegisterButton state={stateValue} />
+            </FusionAuthProvider>,
+        );
 
         fireEvent.click(screen.getByText('Register Now'));
 
-        expect(register).toBeCalledWith('state');
+        expect(register).toHaveBeenCalledWith(stateValue);
     });
 });
-
-const renderProvider = () => {
-    return waitFor(() =>
-        render(
-            <FusionAuthProvider {...TEST_CONFIG}>
-                <FusionAuthRegisterButton state="state" />
-            </FusionAuthProvider>,
-        ),
-    );
-};
